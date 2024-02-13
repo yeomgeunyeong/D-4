c1470d9de925edc2429881080231a9721d0ceccb
yeomgeunyeong <yyynyeong@gmail.com>
2024-02-13 11:47:53 +0900

    02-13

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,5 +1,27 @@
 $(function(){
 
-    
+    $(".main > li").hover(function(){
+        $(".sub").stop().show();
+    }, function(){
+        $(".sub").stop().hide();
+    })
+
+
+
+    $(".fade li").eq(0).siblings().hide();
+    var n=0;    
+    setInterval(function(){
+        n=(n+1)%3;
+        $(".fade li").eq(n).siblings().fadeOut();
+        $(".fade li").eq(n).fadeIn();
+    }, 3000)
+
+
+    $(".p_click").click(function(){
+        $(".pop").fadeIn();
+    })
+    $(".close").click(function(){
+        $(".pop").fadeOut();
+    })
 
 })//
